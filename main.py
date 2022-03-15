e5513f72fd7f4d893aff69d73ded834b76e7c4a5
WindowsVistaisCool <kyle@quackings.com>
2022-03-15 09:20:39 -0400

    UNTESTED more refactoring and add functionality to PathMap

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ wait(30, MSEC)
 #endregion VEXcode Generated Robot Configuration
 
 # ------ Adam and Kyle's code ------
+
+# -- Enums --
 import enum
 class pathDirection(enum.Enum):
     UP = 0
@@ -30,24 +32,13 @@ class pathMapType(enum.Enum):
     LIST = 0
     COORD = 1
 
+# -- Objects --
 class Path:
-    def __init__(self, pathMap: pathMap):
-        self.rawPathMap = pathMap.data()
-        self.unsortedCoords = {}
-        self.coords = {}
-        for row in pathMap.data():
-            if not any(row): continue
-            for order in row:
-                if order != 0:
-                    self.unsortedCoords[order] = (row.index(order), pathMap.index(row))
-        for key in sorted(self.unsortedCoords.keys()):
-            self.coords[key] = self.unsortedCoords[key]
+    def __init__(self, pathMap: PathMap):
+        self.pathMap = pathMap.data()
 
-    def getRawPathMap(self) -> list:
-        return self.rawPathMap
-
-    def getRawCoords(self) -> dict:
-        return self.coords
+    def getRawPathMap(self) -> dict:
+        return self.patMap
 
     def getDirections(self) -> tuple:
         sequentialCommands = []
@@ -67,7 +58,18 @@ class Path:
 class PathMap:
     def __init__(self, type = pathMapType.LIST: pathMapType, pathMap):
         self.type = type
-        self.pathMap = pathMap
+        self.pathMap = {}
+        if self.type == pathMapType.COORDS:
+            self.pathMap = pathMap
+        elif self.type == pathMapType.LIST:
+            unsortedCoords = {}
+            for row in pathMap:
+                if not any(row): continue
+                for order in row:
+                    if order != 0:
+                        unsortedCoords[order] = (row.index(order), pathMap.index(row))
+            for key in sorted(unsortedCoords.keys()):
+                self.pathMap[key] = unsortedCoords[key]
 
     def data(self):
         return self.pathMap
@@ -112,19 +114,19 @@ class Drivetrain:
             currentHeading = direction.value
             self.move_for_tile()
 
+# -- Execution --
 def main():
-    # Define drivtrain to control both motors at once
-    global Drivetrain, Path, pathDirection
+    global Drivetrain, Path, PathMap, pathMapType # "import" everything
     drivetrain = Drivetrain(mLeft, mRight)
 
-    simple_circle = Path([
+    simple_circle = Path(PathMap(pathMapType.LIST, [
                 [9,   8,  7,  6,  5],
                 [10,  0,  0,  0,  4],
                 [11,  0,  0,  0,  3],
                 [12,  0,  0,  0,  2],
                 [13, 14, 15, 16,  1]
-            ])
-    zig_zag = Path([[9, 8, 0, 0, 0], [0, 7, 6, 0, 0], [0, 0, 5, 4, 0], [0, 0, 0, 3, 2], [0, 0, 0, 0, 1]])
+            ])) # Path to rotate around in a circle pattern
+    zig_zag = Path(PathMap(pathMapType.LIST, [[9, 8, 0, 0, 0], [0, 7, 6, 0, 0], [0, 0, 5, 4, 0], [0, 0, 0, 3, 2], [0, 0, 0, 0, 1]])) # Path to zig zag diagonally from bottom right to top left
     drivetrain.run_path(simple_circle)
 
 main()
