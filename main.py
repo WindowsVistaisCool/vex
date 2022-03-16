5a6cf2c453123032f6b74325a3e070b1166d68a0
quackings <101573486+quackings@users.noreply.github.com>
2022-03-16 10:20:10 -0400

    working version, needs a few tweaks

--- main.py
+++ main.py
@@ -20,18 +20,14 @@ wait(30, MSEC)
 # -- Enums --
 class pathDirection:
     UP = 0
-    DOWN = 180
-    RIGHT = 90
-    LEFT = -90
-
-class pathMapType:
-    LIST = 0
-    COORD = 1
+    DOWN = 134
+    RIGHT = 67
+    LEFT = -67
 
 # -- Objects --
 class Path:
-    def __init__(self, pathMap):
-        self.coords = pathMap.data()
+    def __init__(self, pathData):
+        self.coords = pathData.data() if isinstance(pathData, PathMap) else pathData
 
     def getRawCoords(self) -> dict:
         return self.coords
@@ -40,54 +36,50 @@ class Path:
         sequentialCommands = []
         firstCoord = lastCoord = self.coords[1]
         first = True
-        for coord in self.coords.values():
+        brain.screen.print(sorted(self.coords.items()))
+        brain.screen.next_row()
+        for k, coord in sorted(self.coords.items()):
             if first:
                 first = False
                 continue
             # TODO: this logic is probably flawed somehow
-            if coord[1] > lastCoord[1]:
+            if coord[1] < lastCoord[1]:
                 sequentialCommands.append(pathDirection.UP)
-            elif coord[1] < lastCoord[1]:
+            elif coord[1] > lastCoord[1]:
                 sequentialCommands.append(pathDirection.DOWN)
             if coord[0] > lastCoord[0]:
                 sequentialCommands.append(pathDirection.RIGHT)
             elif coord[0] < lastCoord[0]:
                 sequentialCommands.append(pathDirection.LEFT)
             lastCoord = coord
+        brain.screen.print(sequentialCommands)
         return (firstCoord, sequentialCommands)
 
 class PathMap:
-    def __init__(self, pathType, pathMap):
-        self.pathType = pathType
+    def __init__(self, pathMap):
         self.pathMap = {}
-        if self.pathType == 1:
-            for i in range(len(pathMap)):
-                self.pathMap[i + 1] = pathMap[i]
-        elif self.pathType == 0:
-            unsortedCoords = {}
-            for row in pathMap:
-                if not any(row): continue
-                for order in row:
-                    if order != 0:
-                        unsortedCoords[order] = (row.index(order), pathMap.index(row))
-            for key in sorted(unsortedCoords.keys()):
-                self.pathMap[key] = unsortedCoords[key]
+        unsortedCoords = {}
+        for row in pathMap:
+            if not any(row): continue
+            for order in row:
+                if order != 0:
+                    unsortedCoords[order] = (row.index(order), pathMap.index(row))
+        for key in sorted(unsortedCoords.keys()):
+            self.pathMap[key] = unsortedCoords[key]
 
     def data(self):
         return self.pathMap
 
 class Drivetrain:
-    tile_square_size = 8
+    tile_square_size = 12
     tile_center_size = 2.5
-    inch_per_rotation = 7.85
-    tile_size_degrees = (360 / inch_per_rotation) * 8
 
     def __init__(self, drivetrain):
         self.drive = drivetrain
 
     def move_for_tile(self): # TODO: test tile with front color sensor
         self.stopped = False
-        self.drive.drive_for(FORWARD, self.tile_size_degrees, MM)
+        self.drive.drive_for(FORWARD, self.tile_square_size, INCHES)
         self.stopped = True
 
     def rotate(self, degree):
@@ -97,6 +89,8 @@ class Drivetrain:
 
     def run_path(self, path: Path):
         pathData = path.getDirections()
+        brain.screen.next_row()
+        brain.screen.print("Starting path at " + str(pathData[0]))
         directions = pathData[1]
         currentHeading = 0 # TODO: find accurate measurement
         for degree in directions:
@@ -110,15 +104,16 @@ def main():
     global Drivetrain, Path, PathMap # "import" everything
     dt = Drivetrain(drivetrain)
 
-    simple_circle = Path(PathMap(0, [
+    simple_circle = Path(PathMap([
                 [9,   8,  7,  6,  5],
                 [10,  0,  0,  0,  4],
                 [11,  0,  0,  0,  3],
                 [12,  0,  0,  0,  2],
                 [13, 14, 15, 16,  1]
             ])) # Path to rotate around in a circle pattern
-    zig_zag = Path(PathMap(0, [[9, 8, 0, 0, 0], [0, 7, 6, 0, 0], [0, 0, 5, 4, 0], [0, 0, 0, 3, 2], [0, 0, 0, 0, 1]])) # Path to zig zag diagonally from bottom right to top left
-    simple = Path(PathMap(1, [(4, 4), (3, 4)]))
-    dt.run_path(zig_zag)
+    zig_zag = Path({1: (4, 4), 2: (3, 4), 3: (3,3), 4: (2, 3), 5: (2, 2)}) # Path to zig zag diagonally from bottom right to top left
+    simple = Path({1: (4, 4), 2: (3, 4)})
+    # dt.rotate(67)
+    dt.run_path(simple_circle)
 
 main()
